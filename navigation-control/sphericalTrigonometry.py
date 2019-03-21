7149d4ad086b7cf0cfd1d88459b2dd4f68059a13
Eduardo Meléndez <edgame127@gmail.com>
2019-03-20 19:24:36 -0500

    feat: add ECEF and ENU transformation. fix: degreesToRadians

--- navigation-control/sphericalTrigonometry.py
+++ navigation-control/sphericalTrigonometry.py
@@ -1,7 +1,8 @@
 import math
+import numpy as np
 
 def degreesToRadians(degrees):
-    return math.pi / 180
+    return degrees * math.pi / 180
 
 class SphericalPoint():
     def __init__(self, latitude = 0 , longitude = 0):
@@ -13,9 +14,31 @@ class SphericalPoint():
         phi2 = degreesToRadians(destination.latitude)
         delta_lambda = degreesToRadians(destination.longitude - self.longitude)
 
-        x = math.cos( phi1 ) * math.sin( phi2 ) - math.sin( phi1 ) * math.cos( phi2 ) * math.cos( delta_lambda )
-        y = math.sin( delta_lambda ) * math.cos( phi2 )
+        x = np.cos( phi1 ) * np.sin( phi2 ) - np.sin( phi1 ) * np.cos( phi2 ) * np.cos( delta_lambda )
+        y = np.sin( delta_lambda ) * np.cos( phi2 )
 
         theta = math.atan2(y, x)
 
-        return theta    
+        return theta
+    
+    def toECEF(self, radius):
+        phi = degreesToRadians(self.latitude)
+        _lambda = degreesToRadians(self.longitude)
+
+        x = radius * np.cos(phi) * np.cos(_lambda)
+        y = radius * np.cos(phi) * np.sin(_lambda)
+        z = radius * np.sin(phi)
+
+        return np.array([x, y, z])
+
+    def toENU(self, referencePoint, radius):
+        phi_r = degreesToRadians(referencePoint.latitude)
+        lambda_r = degreesToRadians(referencePoint.longitude)
+
+        T = np.array([ 
+            [-np.sin(lambda_r), np.cos(lambda_r), 0],
+            [-np.sin(phi_r) * np.cos(lambda_r), -np.sin(phi_r) * np.sin(lambda_r), np.cos(phi_r)],
+            [ np.cos(phi_r) * np.cos(lambda_r),  np.cos(phi_r) * np.sin(lambda_r), np.sin(phi_r)]
+            ])
+
+        return T @ (self.toECEF(radius) - referencePoint.toECEF(radius))
