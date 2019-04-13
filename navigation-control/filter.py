4b46f4aca42c8406e9bc995662037d09b50821cc
Eduardo Melendez <edgame127@gmail.com>
2019-04-13 12:48:16 -0500

    fix: kallman filter

--- navigation-control/filter.py
+++ navigation-control/filter.py
@@ -1,14 +1,9 @@
 import numpy as np
 
 class KalmanFilter():
-    def __init__(self):
-        self.F = np.array([
-            [1, 0, -speed * dt * np.sin(theta), dt * np.cos(theta), 0],
-            [0, 1,  speed * dt * np.cos(theta), dt * np.sin(theta), 0],
-            [0, 0,                           1,                  0, dt],
-            [0, 0,                           0,                  1, 0],
-            [0, 0,                           0,                  0, 1]
-        ])
+    def __init__(self, robot):
+        self.robot = robot
+
         self.H = np.array([
             [1, 0, 0, 0, 0],
             [0, 1, 0, 0, 0],
@@ -23,7 +18,7 @@ class KalmanFilter():
             [0, 0, 0, 0, 1000]
         ])
 
-        self.Q = Q
+        self.Q = np.identity(5)
 
         self.R = np.array([
             [100, 0, 0],
@@ -31,15 +26,26 @@ class KalmanFilter():
             [0, 0, 25]
         ])
 
-    def predict(self, x):
-        x = f(x, u)
-        self.P = self.F @ self.P @ self.F.transponse() + self.Q
+    def predict(self, x, dt = 0.01):
+
+        speed = self.robot.speed
+        theta = self.robot.theta
+
+        F = np.array([
+            [1, 0, -speed * dt * np.sin(theta), dt * np.cos(theta), 0],
+            [0, 1,  speed * dt * np.cos(theta), dt * np.sin(theta), 0],
+            [0, 0,                           1,                  0, dt],
+            [0, 0,                           0,                  1, 0],
+            [0, 0,                           0,                  0, 1]
+        ])
+        #x = f(x, u)
+        self.P = F @ self.P @ F.transponse() + self.Q
 
         return x
 
     def update(self, z):
 
-        I = np.identity()
+        I = np.identity(5)
 
         y = z - self.H @ x
         S = self.H @ self.P @ self.H.transponse() + self.R
